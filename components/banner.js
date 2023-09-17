5fbba207e79e522d6178b4215608339e22eee3f3
aminoviche <aminoviche@users.noreply.github.com>
2023-09-17 18:45:27 +0100

    Initial commit

---
+++ components/banner.js
@@ -0,0 +1,12 @@
+const Banner = () => {
+  return (
+    <header>
+      <div>
+        <img src='./test.png" alt ="logo' />
+      </div>
+      <div> providing houses all over the world </div>
+    </header>
+  );
+};
+
+export default Banner;
